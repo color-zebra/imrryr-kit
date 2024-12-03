a4c2364be03574d19b14544ef86dae596e39542c
Romanenkov Dmitriy <215rd@mail.ru>
2024-12-03 20:07:35 +0300

    init: start project

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { Button, buttonVariants } from "./components/Button";
+export type { ButtonProps } from "./components/Button";
+export { Alert, AlertDescription, AlertTitle } from "./components/Alert";
